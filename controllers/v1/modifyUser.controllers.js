d4ec2eedb5a7b73fc8270166572daa96ec353b59
Denis Skvortsov <diayuokaro@gmail.com>
2024-03-25 14:25:30 +0300

    Awesome commit

--- controllers/v1/modifyUser.controllers.js
+++ controllers/v1/modifyUser.controllers.js
@@ -3,9 +3,9 @@ import User from '#models/user.models.js'
 export default async (req, res) => {
   const [_, username, passwd] = Buffer.from(req.headers.authorization.replace('Basic ', ''), 'base64').toString('utf-8').match(/(.*):(.*)/)
 
-  const profilePictureBuffer = Buffer.from(req.body.picture.substring(req.body.picture.indexOf(',') + 1))
+  // const profilePictureBuffer = Buffer.from(req.body.picture.substring(req.body.picture.indexOf(',') + 1))
 
-  if (profilePictureBuffer.length <= 12*1024*1024) return
+  // if (profilePictureBuffer.length <= 12*1024*1024) return
 
   if (!await User.validateCredentials(username, passwd)) return res.status(401).send()
   const subject = await User.getByUsername(username)
