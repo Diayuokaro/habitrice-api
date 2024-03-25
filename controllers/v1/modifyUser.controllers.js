81fda58ff2dadec5ab640b5fcdc74404e4b8e56a
Denis Skvortsov <diayuokaro@gmail.com>
2024-03-25 10:46:52 +0300

    Handle big-size Profile Pictures

--- controllers/v1/modifyUser.controllers.js
+++ controllers/v1/modifyUser.controllers.js
@@ -3,6 +3,10 @@ import User from '#models/user.models.js'
 export default async (req, res) => {
   const [_, username, passwd] = Buffer.from(req.headers.authorization.replace('Basic ', ''), 'base64').toString('utf-8').match(/(.*):(.*)/)
 
+  const profilePictureBuffer = Buffer.from(req.body.picture.substring(img.indexOf(',') + 1))
+
+  if (!profilePictureBuffer.length <= 12*1024*1024) return res.status(401).send()
+
   if (!await User.validateCredentials(username, passwd)) return res.status(401).send()
   const subject = await User.getByUsername(username)
 
